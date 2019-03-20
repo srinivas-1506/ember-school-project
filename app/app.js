f844374849a4f5fc36bf695671ce644e44e20a98
srinivas-1506 <vppdb@mst.com>
2019-03-19 20:39:33 -0500

    Inserted a comment

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-
+//This is the change
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
